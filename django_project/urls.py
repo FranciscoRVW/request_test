e51b161f60d0d7ede1e06341737e47f8466644f6
Francisco Ramirez <francisco.rv@wizeline.com>
2022-08-24 13:09:37 -0500

    Redirect root url

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("request_test.urls"))
+    path("", include("request_test.urls"),)
 ]
--- request_test/urls.py
+++ request_test/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import apiV1
 
 urlpatterns = [
+    path("", apiV1)
     path("api/v1/envelope/", apiV1)
 ]
